205c82bae8bdfe794fef4c1ab4529aae767ccfa3
vinicioscastro <viniciospcastro19@gmail.com>
2022-03-13 18:32:39 -0300

    somando dois numeros

--- src/assets/custo.js
+++ src/assets/custo.js
@@ -6,6 +6,7 @@ export default function Custo() {
   const [showName, setShowName] = useState(false);
 
   const [numero1, setNumero1] = useState("");
+  const [numero2, setNumero2] = useState("");
   // const [showNumber1, setShowNuber1] = useState("");
 
   useEffect(() => {
@@ -15,6 +16,7 @@ export default function Custo() {
   return (
     <div className="componentecusto">
       <h1>Teste</h1>
+
       <b>Nome</b>
       <br />
       <input
@@ -24,10 +26,12 @@ export default function Custo() {
         onChange={(event) => setName(event.target.value)}
       />
       <br />
+
       <button onClick={() => setShowName(true)}>enviar</button>
       <br />
       <br />
       {showName && <h1>{name}</h1>}
+
       <b>Digite 1 numero</b>
       <input
         type="number"
@@ -36,6 +40,23 @@ export default function Custo() {
         onChange={(event) => setNumero1(event.target.value)}
       />
       {console.log(numero1)}
+      <br></br>
+      <b>Digite 2 numero</b>
+      <input
+        type="number"
+        name="numero2"
+        value={numero2}
+        onChange={(event) => setNumero2(event.target.value)}
+      />
+      {console.log(numero2)}
+      <br></br>
+      <button
+        onClick={(event) => {
+          console.log(numero1 * numero2);
+        }}
+      >
+        Multiplicar os 2 numeros
+      </button>
     </div>
   );
 }
