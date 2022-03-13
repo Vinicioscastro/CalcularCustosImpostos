5279f5631a8b29d8d51091abeb080c8c85c13731
vinicioscastro <viniciospcastro19@gmail.com>
2022-03-13 18:13:38 -0300

    usando useState

--- src/assets/custo.js
+++ src/assets/custo.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./custo.css";
 
 export default function Custo() {
+  const [name, setName] = useState("");
+  const [showName, setShowName] = useState(false);
 
-    var nome;
+  const [numero1, setNumero1] = useState("");
+  // const [showNumber1, setShowNuber1] = useState("");
+
+  useEffect(() => {
+    setShowName(false);
+  }, [name]);
 
   return (
     <div className="componentecusto">
-    
       <h1>Teste</h1>
       <b>Nome</b>
       <br />
-      <input type="text" name="nome" value={nome}/>
-      
-      <button onClick={console.log("vinicios")}>
-          enviar
-      </button>
+      <input
+        type="text"
+        name="nome"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <br />
+      <button onClick={() => setShowName(true)}>enviar</button>
       <br />
       <br />
+      {showName && <h1>{name}</h1>}
+      <b>Digite 1 numero</b>
+      <input
+        type="number"
+        name="numero1"
+        value={numero1}
+        onChange={(event) => setNumero1(event.target.value)}
+      />
+      {console.log(numero1)}
     </div>
   );
 }
