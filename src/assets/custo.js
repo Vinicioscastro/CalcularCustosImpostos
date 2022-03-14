1648c5306fe4423212baf617d9bd0608bf6c95af
vinicioscastro <viniciospcastro19@gmail.com>
2022-03-14 14:54:06 -0300

    organizando os campos

--- src/assets/custo.js
+++ src/assets/custo.js
@@ -4,10 +4,12 @@ import "./custo.css";
 export default function Custo() {
   const [name, setName] = useState("");
   const [showName, setShowName] = useState(false);
-
-  const [numero1, setNumero1] = useState("");
-  const [numero2, setNumero2] = useState("");
-  // const [showNumber1, setShowNuber1] = useState("");
+  const [soma, setSoma] = useState(0);
+  const [maodeobra, setMaodeobra] = useState(null);
+  const [host, setHost] = useState(null);
+  const [contador, setContador] = useState(null);
+  const [iss, setIss] = useState(null);
+  const [imposto, setImposto] = useState(null);
 
   useEffect(() => {
     setShowName(false);
@@ -15,48 +17,63 @@ export default function Custo() {
 
   return (
     <div className="componentecusto">
-      <h1>Teste</h1>
+      <h1>Entre com os dados necessarios</h1>
 
       <b>Nome</b>
-      <br />
       <input
         type="text"
         name="nome"
         value={name}
         onChange={(event) => setName(event.target.value)}
       />
-      <br />
 
       <button onClick={() => setShowName(true)}>enviar</button>
-      <br />
-      <br />
+
       {showName && <h1>{name}</h1>}
 
-      <b>Digite 1 numero</b>
+      <b>Mão de Obra</b>
       <input
         type="number"
-        name="numero1"
-        value={numero1}
-        onChange={(event) => setNumero1(event.target.value)}
+        name="maodeobra"
+        value={maodeobra}
+        onChange={(event) => setMaodeobra(Number(event.target.value))}
       />
-      {console.log(numero1)}
-      <br></br>
-      <b>Digite 2 numero</b>
+
+      <b>Serviços de hospedagem</b>
       <input
         type="number"
-        name="numero2"
-        value={numero2}
-        onChange={(event) => setNumero2(event.target.value)}
+        name="host"
+        value={host}
+        onChange={(event) => setHost(Number(event.target.value))}
       />
-      {console.log(numero2)}
-      <br></br>
+
+      <b>Contabilidade</b>
+      <input
+        type="number"
+        name="contador"
+        value={contador}
+        onChange={(event) => setContador(Number(event.target.value))}
+      ></input>
+      <b>Imposto Iss</b>
+      <input
+        type="Number"
+        name="iss"
+        value={iss}
+        onChange={(event) => setIss(Number(event.target.value))}
+      ></input>
+
+      <b>Imposto Sobre Lucro</b>
+      <input type="number"></input>
+
       <button
         onClick={(event) => {
-          console.log(numero1 * numero2);
+          setSoma(maodeobra + host);
         }}
       >
-        Multiplicar os 2 numeros
+        Somar os 2 numeros
       </button>
+
+      {soma > 0 && <h1>{soma}</h1>}
     </div>
   );
 }
